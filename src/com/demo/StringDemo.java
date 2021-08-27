d54dca4f9a70f9f0d3369e9b96f76acbfec40899
prakashthore <87518422+prakashthore@users.noreply.github.com>
2021-08-27 16:03:17 +0530

    Update StringDemo.java

--- src/com/demo/StringDemo.java
+++ src/com/demo/StringDemo.java
@@ -5,6 +5,9 @@ public class StringDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello string project..");
+		String str= "prakash";
+		System.out.println(str);
+
 
 	}
 
